f4a6bc31bd7be8664ca12a254aaf3ad336544b09
Gibeom Choi <kcdevdes@gmail.com>
2025-02-24 15:37:10 -0500

    Initial commit

---
+++ src/main/java/org/umbkosa/kcdevdes/UmbKosaApplication.java
@@ -0,0 +1,13 @@
+package org.umbkosa.kcdevdes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmbKosaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmbKosaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/umbkosa/kcdevdes/UmbKosaApplicationTest.java
@@ -0,0 +1,8 @@
+package org.umbkosa.kcdevdes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UmbKosaApplicationTest {
+}
